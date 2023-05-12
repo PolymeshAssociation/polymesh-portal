29c9361880d131d8d350899e001c24ff84ad70e1
Francis O'Brien <42175565+F-OBrien@users.noreply.github.com>
2023-05-12 11:37:27 +0100

    Authorizations page fixes (#13)
    * show account auths when account has identity
    
    * fix authorization permissions in details

--- src/layouts/Authorizations/components/AuthorizationItem/helpers.tsx
+++ src/layouts/Authorizations/components/AuthorizationItem/helpers.tsx
@@ -2,7 +2,12 @@ import { HumanReadable } from '@polymeshassociation/polymesh-sdk/api/entities/Au
 import {
   AuthorizationRequest,
   AuthorizationType,
+  DefaultPortfolio,
+  JoinIdentityAuthorizationData,
+  Permissions,
+  PolymeshError,
   PortfolioCustodyAuthorizationData,
+  RotatePrimaryKeyToSecondaryData,
 } from '@polymeshassociation/polymesh-sdk/types';
 import { NumberedPortfolio } from '@polymeshassociation/polymesh-sdk/internal';
 import { CopyToClipboard } from '~/components';
@@ -14,6 +19,135 @@ import {
 } from './styles';
 import { formatDid } from '~/helpers/formatters';
 
+function getTickerPermissions(permissions: Permissions) {
+  const tickerPermission =
+    permissions.assets?.type === 'Exclude'
+      ? 'Excluded Tickers'
+      : 'Included Tickers';
+
+  let tickerPermissionDetails: string[];
+  if (permissions.assets) {
+    if (permissions.assets.values.length) {
+      tickerPermissionDetails = permissions.assets.values.map((asset) => {
+        return asset.ticker;
+      });
+    } else {
+      tickerPermissionDetails = ['None'];
+    }
+  } else {
+    tickerPermissionDetails = ['All'];
+  }
+
+  return {
+    permission: tickerPermission,
+    type: permissions.assets ? permissions.assets.type : 'Include',
+    details: tickerPermissionDetails,
+  };
+}
+
+async function getPortfolioName(
+  portfolio: NumberedPortfolio | DefaultPortfolio,
+): Promise<string> {
+  if (portfolio instanceof NumberedPortfolio) {
+    try {
+      const name = await portfolio.getName();
+      return `${portfolio.id.toString()} / ${name}`;
+    } catch (error) {
+      if ((error as PolymeshError).message === "The Portfolio doesn't exist") {
+        return `${portfolio.id.toString()} / Non-existent portfolio`;
+      }
+      throw error;
+    }
+  }
+  return 'Default';
+}
+
+async function getPortfolioPermissions(permissions: Permissions) {
+  const portfolioPermission =
+    permissions.portfolios?.type === 'Exclude'
+      ? 'Excluded Portfolios'
+      : 'Included Portfolios';
+
+  let portfolios: string[];
+  if (permissions.portfolios) {
+    if (permissions.portfolios.values.length) {
+      portfolios = await Promise.all(
+        permissions.portfolios.values.map(async (portfolio) => {
+          const shortDid = formatDid(portfolio.owner.did, 6, 7);
+          const portfolioIdAndName = await getPortfolioName(portfolio);
+          return `${shortDid} / ${portfolioIdAndName}`;
+        }),
+      );
+    } else {
+      portfolios = ['None'];
+    }
+  } else {
+    portfolios = ['All'];
+  }
+
+  return {
+    permission: portfolioPermission,
+    type: permissions.portfolios ? permissions.portfolios.type : 'Include',
+    details: portfolios,
+  };
+}
+
+function getTransactionPermissions(permissions: Permissions) {
+  let primaryPermissionType: string;
+  let exceptionPermissionType: string;
+  if (permissions.transactions?.type === 'Exclude') {
+    primaryPermissionType = 'Excluded Transactions';
+    exceptionPermissionType = 'Exceptions (included)';
+  } else {
+    primaryPermissionType = 'Included Transactions';
+    exceptionPermissionType = 'Exceptions (excluded)';
+  }
+
+  let transactionsPermissions: string[];
+  if (permissions.transactions) {
+    if (permissions.transactions.values.length) {
+      transactionsPermissions = permissions.transactions.values.map((value) => {
+        if (!value.includes('.')) {
+          return `all ${value} transactions`;
+        }
+        return value;
+      });
+    } else {
+      transactionsPermissions = ['None'];
+    }
+  } else {
+    transactionsPermissions = ['All'];
+  }
+
+  let exceptions: string[];
+  if (permissions.transactions) {
+    if (permissions.transactions.exceptions?.length) {
+      exceptions = permissions.transactions.exceptions;
+    } else {
+      exceptions = ['None'];
+    }
+  } else {
+    exceptions = ['None'];
+  }
+
+  return [
+    {
+      permission: primaryPermissionType,
+      type: permissions.transactions
+        ? permissions.transactions.type
+        : 'Include',
+      details: transactionsPermissions,
+    },
+    {
+      permission: exceptionPermissionType,
+      type: permissions.transactions
+        ? permissions.transactions.type
+        : 'Exclude',
+      details: exceptions,
+    },
+  ];
+}
+
 const parseDetails = async (
   staticData: HumanReadable,
   rawData: AuthorizationRequest,
@@ -21,75 +155,43 @@ const parseDetails = async (
   const details = staticData.data;
 
   switch (details.type) {
-    case AuthorizationType.JoinIdentity:
+    case AuthorizationType.JoinIdentity: {
+      const permissions = (rawData.data as JoinIdentityAuthorizationData).value;
+      const txPermissions = getTransactionPermissions(permissions);
       return [
-        {
-          permission: details.value.assets ? 'Ticker' : null,
-          type: details.value.assets ? details.value.assets.type : null,
-          details: details.value.assets ? details.value.assets.values : [],
-        },
-        {
-          permission: details.value.portfolios ? 'Portfolio(s)' : null,
-          type: details.value.portfolios ? details.value.portfolios.type : null,
-          details: details.value.portfolios
-            ? details.value.portfolios.values.map(({ did }) => did)
-            : [],
-        },
-        {
-          permission: details.value.transactions ? 'Extrinsics' : null,
-          type: details.value.transactions
-            ? details.value.transactions.type
-            : null,
-          details: details.value.transactions
-            ? details.value.transactions.values
-            : [],
-        },
+        getTickerPermissions(permissions),
+        await getPortfolioPermissions(permissions),
+        txPermissions[0],
+        txPermissions[1],
       ];
+    }
 
     case AuthorizationType.RotatePrimaryKey:
       return [];
 
-    case AuthorizationType.RotatePrimaryKeyToSecondary:
+    case AuthorizationType.RotatePrimaryKeyToSecondary: {
+      const permissions = (rawData.data as RotatePrimaryKeyToSecondaryData)
+        .value;
+      const txPermissions = getTransactionPermissions(permissions);
+
       return [
-        {
-          permission: details.value.assets ? 'Ticker' : null,
-          type: details.value.assets ? details.value.assets.type : null,
-          details: details.value.assets ? details.value.assets.values : [],
-        },
-        {
-          permission: details.value.portfolios ? 'Portfolio(s)' : null,
-          type: details.value.portfolios ? details.value.portfolios.type : null,
-          details: details.value.portfolios
-            ? details.value.portfolios.values.map(({ did }) => did)
-            : [],
-        },
-        {
-          permission: details.value.transactions ? 'Extrinsics' : null,
-          type: details.value.transactions
-            ? details.value.transactions.type
-            : null,
-          details: details.value.transactions
-            ? details.value.transactions.values
-            : [],
-        },
+        getTickerPermissions(permissions),
+        await getPortfolioPermissions(permissions),
+        txPermissions[0],
+        txPermissions[1],
       ];
+    }
 
     case AuthorizationType.PortfolioCustody:
-      // eslint-disable-next-line no-case-declarations
-      const targetPortfolio = (
-        rawData.data as PortfolioCustodyAuthorizationData
-      ).value;
-
       return [
         {
           permission: 'Target Portfolio',
           type: null,
-          details:
-            targetPortfolio instanceof NumberedPortfolio
-              ? [
-                  `${await targetPortfolio.getName()} / ${targetPortfolio.id.toString()}`,
-                ]
-              : ['Default / 0'],
+          details: [
+            await getPortfolioName(
+              (rawData.data as PortfolioCustodyAuthorizationData).value,
+            ),
+          ],
         },
         {
           permission: 'Target DID',
@@ -164,36 +266,24 @@ export const renderDetails = async (
 ) => {
   const parsedDetails = await parseDetails(staticData, rawData);
 
-  if (
-    parsedDetails.every(
-      ({ permission, type }) => permission === null && type === null,
-    )
-  ) {
-    return null;
-  }
-
   return parsedDetails.length ? (
     <StyledDetailsWrapper>
-      {parsedDetails.map(({ permission, type, details }) =>
+      {parsedDetails.map(({ permission, details }) =>
         permission ? (
           <StyledDetailItem key={permission}>
             {permission}:
-            {details.length ? (
-              details.map((detail) => (
-                <StyledDetailValue key={detail}>
-                  {detail.length === 66 || detail.length === 48 ? (
-                    <>
-                      {formatDid(detail, 6, 7)}
-                      <CopyToClipboard value={detail} />
-                    </>
-                  ) : (
-                    detail
-                  )}
-                </StyledDetailValue>
-              ))
-            ) : (
-              <StyledDetailValue>{type} all</StyledDetailValue>
-            )}
+            {details.map((detail) => (
+              <StyledDetailValue key={detail}>
+                {detail.length === 66 || detail.length === 48 ? (
+                  <>
+                    {formatDid(detail, 6, 7)}
+                    <CopyToClipboard value={detail} />
+                  </>
+                ) : (
+                  detail
+                )}
+              </StyledDetailValue>
+            ))}
           </StyledDetailItem>
         ) : null,
       )}
