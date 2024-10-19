658b6172c1fb1243c1588d9ec518dc1cf9c20eeb
F-OBrien <francis@polymesh.network>
2024-10-19 16:00:59 +0100

    add eslint-plugin-deprecation + bump to sdk to v26.0.0-beta.1

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:deprecation/recommended',
   ],
   overrides: [],
   parser: '@typescript-eslint/parser',
